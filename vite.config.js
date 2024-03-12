6c4f9f89f6f726bcffcae7c9798cd66f1175e8dd
iflytek_xuchenli <clxu7@iflytek.com>
2024-03-12 16:55:19 +0800

    chore: 引入vite-plugin-build依赖构建多文件

--- vite.config.js
+++ vite.config.js
@@ -6,36 +6,55 @@ import {
   resolve
 } from 'path'
 import {
-  libInjectCss,
-} from 'vite-plugin-lib-inject-css';
+  buildPlugin
+} from 'vite-plugin-build'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 const _resolve = (path) => resolve(__dirname, path)
 
-export default ({
-  mode
-}) => defineConfig({
-  plugins: [vue(), libInjectCss()],
+export default defineConfig({
+  plugins: [vue(), cssInjectedByJsPlugin({
+    topExecutionPriority: false
+  }), buildPlugin({
+    fileBuild: false,
+    libBuild: {
+      buildOptions: [{
+          lib: {
+            entry: _resolve('src/lib/index.js'),
+            name: 'index',
+            formats: ['es', 'umd'],
+            fileName: (mode) => `index.${mode}.js`,
+          },
+        }, {
+          lib: {
+            entry: _resolve('src/lib/SlotMachine/index.js'),
+            name: 'SlotMachine',
+            formats: ['es', 'umd'],
+            fileName: (mode) => `SlotMachine.${mode}.js`,
+          },
+        },
+        {
+          lib: {
+            entry: _resolve('src/lib/Lottery/lottery-grid.js'),
+            name: 'LotteryGrid',
+            formats: ['es', 'umd'],
+            fileName: (mode) => `LotteryGrid.${mode}.js`,
+          },
+        },
+        {
+          lib: {
+            entry: _resolve('src/lib/Lottery/lottery-list.js'),
+            name: 'LotteryList',
+            formats: ['es', 'umd'],
+            fileName: (mode) => `LotteryList.${mode}.js`,
+          },
+        },
+      ],
+    },
+  })],
   resolve: {
     alias: {
       '@': _resolve('src')
     }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        // Put chunk files at <output>/chunks
-        chunkFileNames: 'chunks/[name].[hash].js',
-        // Put chunk styles at <output>/styles
-        assetFileNames: 'assets/[name][extname]',
-      },
-    },
-    lib: {
-      entry: {
-        index: _resolve('src/lib/index.js'), // Don't forget the main entry!
-        SlotMachine: _resolve('src/lib/SlotMachine/index.js'),
-        LotteryGrid: _resolve('src/lib/Lottery/lottery-grid.js'),
-        LotteryList: _resolve('src/lib/Lottery/lottery-list.js'),
-      },
-    },
   },
 })
